28cdab283e51e9c3d5a5f61aa8fb1dd5c259c832
parsharm <parsharm@redhat.com>
2021-03-19 11:23:46 +0530

    SET-338 [mjolnir] Record which organizations/teams a user has been unsubscribed from

---
+++ domain/src/main/java/org/jboss/set/mjolnir/archive/domain/repositories/UnsubscribeStatus.java
@@ -0,0 +1,14 @@
+package org.jboss.set.mjolnir.archive.domain.repositories;
+
+public enum UnsubscribeStatus {
+
+    /**
+     * Unsubscribe Process has been completed.
+     */
+    COMPLETED,
+
+    /**
+     * Unsubscribe Process has been failed.
+     */
+    FAILED
+}
---
+++ domain/src/main/java/org/jboss/set/mjolnir/archive/domain/repositories/UnsubscribedUsersFromOrgs.java
@@ -0,0 +1,44 @@
+package org.jboss.set.mjolnir.archive.domain.repositories;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.jboss.set.mjolnir.archive.domain.UserRemoval;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "unsubscribed_users_from_orgs")
+public class UnsubscribedUsersFromOrgs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "unsubscribed_users_from_orgs_generator")
+    @SequenceGenerator(name = "unsubscribed_users_from_orgs_generator", sequenceName = "sq_unsubscribed_users_from_orgs", allocationSize = 1)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_removal_id")
+    private UserRemoval userRemoval;
+
+    @Column(name = "github_username")
+    private String githubUsername;
+
+    @Column(name = "github_org_name")
+    private String githubOrgName;
+    
+    @Enumerated(EnumType.STRING)
+    private UnsubscribeStatus status;
+
+    @CreationTimestamp
+    private Timestamp created;
+
+}
